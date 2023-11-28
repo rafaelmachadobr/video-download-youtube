f3f116a26fdc116613302b5076a1f07bb8963538
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-11-28 13:53:39 -0300

    refactor: audio and video downloaders

--- service/video_downloader.py
+++ service/video_downloader.py
@@ -1,9 +1,26 @@
 from pytube import YouTube
 from pytube.exceptions import RegexMatchError
+from typing import Optional
 
 
 class VideoDownloader:
-    def get_video_info(self, url):
+    """
+    Classe responsável por baixar um vídeo do YouTube.
+    """
+
+    def get_video_info(self, url: str) -> Optional[YouTube]:
+        """
+        Obtém informações sobre um vídeo do YouTube.
+
+        Parâmetros:
+        - url (str): URL do vídeo do YouTube.
+
+        Exceções:
+        - RegexMatchError: Se não for possível fazer correspondência com o padrão do vídeo.
+
+        Retorna:
+        Um objeto YouTube contendo informações sobre o vídeo, ou None se o vídeo não for encontrado.
+        """
         try:
             return YouTube(url)
         except RegexMatchError:
